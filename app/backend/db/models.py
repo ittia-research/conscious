49e07942675cd2557bc26366fd2d296a1f04cc6c
etwk <48991073+etwk@users.noreply.github.com>
2025-04-05 12:54:43 +0800

    [Database] add SRS fields to table thoughts

--- app/backend/db/models.py
+++ app/backend/db/models.py
@@ -1,18 +1,42 @@
-import uuid
-from sqlalchemy import Column, BigInteger, String, Text, TIMESTAMP, func
-from sqlalchemy_utils import UUIDType
+import enum
+from sqlalchemy import Column, Integer, BigInteger, Boolean, DateTime, Float, Text, TIMESTAMP, func, Enum as SQLAlchemyEnum
 from pgvector.sqlalchemy import VECTOR
+from fsrs import State as FSRSState
 
 from .session import Base
 from core.config import settings
 
-
+# TO-DO: is it a good practice to use FSRSState value in database level directly?
 class Thoughts(Base):
     __tablename__ = "thoughts"
     thought_id = Column(BigInteger, primary_key=True)
     text = Column(Text, nullable=False)
     embedding = Column(VECTOR(settings.VECTOR_DIMENSION), nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
+    # SRS Fields
+    srs_due = Column(DateTime(timezone=True), index=True)
+    srs_stability = Column(Float)
+    srs_difficulty = Column(Float)
+    srs_lapses = Column(Integer, default=0)
+    _srs_state = Column("srs_state", Integer)
+    srs_last_review = Column(DateTime(timezone=True))
+    srs_reps = Column(Integer, default=0)
+    srs_discarded = Column(Boolean)
+
+    @property
+    def srs_state(self) -> FSRSState:
+        """Gets the state as a FSRSState enum member."""
+        if not self._srs_state: # srs_state could be Null
+            return self._srs_state
+        return FSRSState(self._srs_state)
+
+    @srs_state.setter
+    def srs_state(self, value: FSRSState):
+        """Sets the state using a TaskStatus enum member."""
+        if not isinstance(value, FSRSState):
+            raise TypeError(f"Expected TaskStatus enum member, got {type(value)}")
+        self._srs_state = value.value # Store the integer value
 
     def __repr__(self):
         _limit = 50
--- app/database/initdb.d/01-create-tables.sql
+++ app/database/initdb.d/01-create-tables.sql
@@ -1,13 +1,26 @@
 -- Create the 'thoughts' table
+-- SRS stands for Spaced Repetition Scheduler
 CREATE TABLE IF NOT EXISTS thoughts (
     thought_id BIGSERIAL PRIMARY KEY,       -- Use BIGSERIAL for potentially large tables
     text TEXT NOT NULL,                     -- Text content
     embedding VECTOR(1536) NOT NULL,        -- Vector embedding. Replace the dimension number.
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP -- Timestamp when the record was created
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the record was created
+
+    -- SRS columns
+    srs_due TIMESTAMP WITH TIME ZONE,       -- In UTC, When the card is next due for review
+    srs_stability FLOAT,
+    srs_difficulty FLOAT,
+    srs_lapses INTEGER DEFAULT 0,           -- Number of times forgotten
+    srs_state INTEGER,                      -- Mapped to FSRS states
+    srs_last_review TIMESTAMP WITH TIME ZONE, -- When the card was last reviewed
+    srs_reps INTEGER DEFAULT 0;             -- Number of times reviewed
+    srs_discarded BOOLEAN                   -- If this row is discarded for SRS 
 );
+-- Create indexes
+CREATE INDEX idx_thoughts_srs_due ON thoughts (srs_due); -- For querying of due cards
 
 -- Create a StreamingDiskANN index on embedding for faster similarity search 
-CREATE INDEX thoughts_idx ON thoughts USING diskann (embedding vector_cosine_ops);  -- cosine distance
+CREATE INDEX idx_thoughts_embedding ON thoughts USING diskann (embedding vector_cosine_ops);  -- cosine distance
 
 -- Setup graph --
 CREATE EXTENSION IF NOT EXISTS age;
