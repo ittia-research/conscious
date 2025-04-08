fed4dcec77538e1efd617082858fb55e3576ac39
etwk <48991073+etwk@users.noreply.github.com>
2025-04-08 21:04:29 +0800

    [Frontend, Infra] fix websocket start in production

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 import { vitePluginTrpcWebSocket } from 'trpc-sveltekit/websocket'; 
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), vitePluginTrpcWebSocket]
-});
+const config: UserConfig = {
+	plugins: [
+		tailwindcss(), 
+		sveltekit(), 
+		vitePluginTrpcWebSocket
+	]
+};
+
+export default config;
